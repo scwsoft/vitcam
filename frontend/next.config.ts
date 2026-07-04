9f9e0153bb850ae34af1aacb502dcf06e20f4fe1
DESKTOP-13CTR2R\sean <scwoods@gmail.com>
2026-07-05 00:57:31 +0800

    no message

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -30,6 +30,18 @@ const nextConfig: NextConfig = {
         port: '54321',
         pathname: '/storage/**',
       },
+       {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**',
+      },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
